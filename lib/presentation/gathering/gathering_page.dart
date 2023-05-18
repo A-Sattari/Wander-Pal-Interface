b57fd153da6b758a59b55c035953f0c1c46d53c0
Alireza <A.Sattari@outlook.com>
2023-05-18 00:48:59 -0500

    Add more details to the Gathering page

--- lib/presentation/gathering/gathering_page.dart
+++ lib/presentation/gathering/gathering_page.dart
@@ -12,7 +12,61 @@ class _GatheringPageState extends State<GatheringPage> {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: <Widget>[GatheringsHighlightList()],
+      children: [
+        Flexible(
+          child: ListView(
+            children: const [
+              Text("Gatherings in ..."),
+            ],
+          ),
+        ),
+        // The Bottom Section
+        Flexible(
+          flex: 8,
+          child: Container(
+            decoration: _getBottomSectionDesign(),
+            child: Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(top: 20, right: 10, left: 10),
+                  child: GatheringsHighlightList(),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 20, bottom: 40, right: 10, left: 10),
+                  child: GatheringsHighlightList(),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  child: const Text("See more"),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
+  }
+
+  BoxDecoration _getBottomSectionDesign() {
+    return 
+      BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 10,
+            offset: const Offset(0, -10),
+          ),
+        ],
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        ),
+      );
   }
 }
--- lib/presentation/gathering/gatherings_highlight_list.dart
+++ lib/presentation/gathering/gatherings_highlight_list.dart
@@ -12,45 +12,42 @@ class GatheringsHighlightList extends StatefulWidget {
 class _GatheringsHighlightListState extends State<GatheringsHighlightList> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(right: 10.0, left: 10.0),
-      child: ExpansionTile(
-        maintainState: true,
-        initiallyExpanded: true,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: const [
-            Text(
-              "Happening Today",
-              textAlign: TextAlign.left,
-            ),
-            Text(
-              "2020-4-11",
-              textAlign: TextAlign.right,
-            ),
-          ],
-        ),
-        children: <Widget>[
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: <Widget>[
-                GatheringCard(
-                  "Title",
-                  "Description",
-                  DateTime(2023, 1, 12, 10, 30),
-                ),
-                GatheringCard(
-                  "Title",
-                  "Description",
-                  DateTime(2023, 1, 12, 10, 30),
-                  endDateTime: DateTime(2023, 2, 12, 15, 30),
-                ),
-              ],
-            ),
+    return ExpansionTile(
+      maintainState: true,
+      initiallyExpanded: true,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: const [
+          Text(
+            "Happening Today",
+            textAlign: TextAlign.left,
+          ),
+          Text(
+            "2020-4-11",
+            textAlign: TextAlign.right,
           ),
         ],
       ),
+      children: <Widget>[
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: <Widget>[
+              GatheringCard(
+                "Title",
+                "Description",
+                DateTime(2023, 1, 12, 10, 30),
+              ),
+              GatheringCard(
+                "Title",
+                "Description",
+                DateTime(2023, 1, 12, 10, 30),
+                endDateTime: DateTime(2023, 2, 12, 15, 30),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
