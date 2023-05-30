b57b4a37251ea0e6a9499bb0ab2d97bbf72614c1
Alireza <A.Sattari@outlook.com>
2023-05-30 12:54:50 -0500

    Auth token is stored in Secure Storage

--- lib/services/account_service.dart
+++ lib/services/account_service.dart
@@ -3,12 +3,17 @@ import "package:firebase_auth/firebase_auth.dart";
 import "package:google_sign_in/google_sign_in.dart";
 import "package:wander_pal/services/api_client.dart";
 import "package:flutter_facebook_auth/flutter_facebook_auth.dart";
+import "package:flutter_secure_storage/flutter_secure_storage.dart";
 
 class AccountService {
 
+  static const String idpTokenKey = "Bearer Token";
+
   Future<void> setupAccount(UserCredential credential) async {
     final authUser = credential.user; //TODO: Throw exception if User is null
-    final idpToken = await authUser!.getIdToken(); //TODO: Store the token locally
+    final idpToken = await authUser!.getIdToken();
+    const secureStorage = FlutterSecureStorage();
+    secureStorage.write(key: idpTokenKey, value: idpToken);
 
     final apiClient = APIClient();
     String userQuery =
